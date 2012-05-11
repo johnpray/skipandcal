1654e9c9f984804e698a00d66c850bd9d0bdec6d
John Pray <louiegeetoo@gmail.com>
2012-05-11 16:52:43 -0400

    Page control tweaks to hide when appropriate. Get rid of about page. I think we're just about ready for the first launch.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Skipandcal::Application.routes.draw do
 
   root to: redirect('/comics')
 
-  match '/about', to: 'static_pages#about'
   match '/login', to: 'sessions#new'
   match '/logout', to: 'sessions#destroy', via: :delete
 
