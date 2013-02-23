958ed63cf3538c47a8bbfec61785f8217e59e8ac
John P. Pray <louiegeetoo@gmail.com>
2013-02-23 17:19:56 -0500

    Fix title helper so it can handle 0 arguments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
-  def full_title(page_title)
+  def full_title(page_title = '')
     base_title = "Skip & Cal ~ cartoons and comics by John Pray"
     if page_title.empty?
       base_title
